f52e7b9ae04c25126c1bf2677f13c87c12937022
Tom Costa <ewerton.costa@metaltoad.com>
2025-04-30 13:44:10 -0300

    fix ControllerAdvice to work with swagger and removing deploy step from github workflow

--- src/main/kotlin/com/currencyconverter/advice/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/currencyconverter/advice/GlobalExceptionHandler.kt
@@ -1,11 +1,13 @@
 package com.currencyconverter.advice
 
 import com.currencyconverter.exception.InvalidCurrencyCodeException
+import io.swagger.v3.oas.annotations.Hidden
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
+@Hidden
 @ControllerAdvice
 class GlobalExceptionHandler {
 
